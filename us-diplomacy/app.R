6573c9912dbfa57919f05c3b53a2cae8c1a4ff04
sfan95 <sherryfansf@berkeley.edu>
2020-08-01 09:24:13 -0700

    finished project

---
+++ us-diplomacy/app.R
@@ -0,0 +1,99 @@
+library(shiny)
+library(leaflet)
+source("utils.R")
+
+ui <- bootstrapPage(
+    tags$head(
+        tags$style(HTML(".help-block {color: #FFFFFF !important;
+                                      font-size: 20px;}")), 
+        tags$style(type = "text/css", "html, body {width:120%;height:120%}"), 
+        tags$style(HTML(
+            "label {color: #FFFFFF;}"
+        )),
+        tags$style(
+            "#errorMsg {
+                color: #FFFFFF;
+                font-size: 20px;
+            }"
+        )
+    ),
+    leafletOutput("map", width = "100%", height = "100%"),
+    absolutePanel(top = 10, right = 10,
+                  helpText("I want to learn about US diplomacy", color="cyan"),
+                  selectInput("byCountry", "in: ",
+                              c("", allCountries), selected = ""
+                  ),
+                  selectInput("byTopic", "relating to: ",
+                              c("", allTopics), selected = ""
+                  )
+    ),
+    absolutePanel(top = 10, left = 50, 
+                  textOutput("errorMsg"))
+)
+
+server <- function(input, output, session) {
+    circleData <- reactive({
+        if (input$byCountry != "") {
+            return(connectedCountries(input$byCountry))
+        } else {
+            return(embassies)
+        }
+    })
+    
+    polylinedata <- reactive({
+        if (input$byCountry == "" & input$byTopic == "") {
+            return(data.frame(lat=c(1, 1), lon=c(1, 1)))
+        } else if (input$byCountry == "") {
+            onTopic <- topicCountries(embassies, input$byTopic)
+            allConnectedonTopic <- connectionsDF(uniqueListCol(onTopic$Connections), 
+                                                 onTopic[1, 3], onTopic[1, 4])
+            temp <- onTopic[, 3:4]
+            names(temp) <- c("lat", "lon")
+            topicConnections <- rbind(temp, allConnectedonTopic)
+            return(topicConnections)
+        } else if (input$byTopic == "") {
+            correctCountry <- embassies[embassies$Country == input$byCountry,]
+            countryConnections <- connectionsDF(uniqueListCol(correctCountry$Connections), 
+                                                correctCountry[1, 3], correctCountry[1, 4])
+            countryLatLon <- correctCountry[, 3:4]
+            names(countryLatLon) <- c("lat", "lon")
+            return(rbind(countryLatLon, countryConnections))
+        } else {
+            correctCountry <- embassies[embassies$Country == input$byCountry,]
+            onTopicWithinCountry <- topicCountries(correctCountry, input$byTopic)
+            connectCountryTopic <- connectionsDF(uniqueListCol(onTopicWithinCountry$Connections),
+                                                 onTopicWithinCountry[1, 3],
+                                                 onTopicWithinCountry[1, 4])
+            countryLatLon <- onTopicWithinCountry[, 3:4]
+            names(countryLatLon) <- c("lat", "lon")
+            topicAndCountryFiltered <- rbind(countryLatLon, connectCountryTopic)
+            return(topicAndCountryFiltered[complete.cases(topicAndCountryFiltered),])
+        }
+    })
+    
+    output$map <- renderLeaflet({
+        leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels)
+    })
+    
+    observe({
+        leafletProxy("map") %>%
+            clearShapes() %>%
+            addPolylines(data = polylinedata(), lat = ~lat, lng= ~lon, weight = 2, color = "blue")
+    })
+    
+    observe({
+        leafletProxy("map") %>%
+            clearMarkers() %>%
+            addCircleMarkers(data = circleData(), ~Longitude, ~Latitude, popup = ~as.character(Mission),
+                             label = ~as.character(Property.Name), radius = 5, color = "cyan",
+                             stroke = FALSE, fillOpacity = 0.8)
+    })
+    
+    output$errorMsg <- renderText({
+        if (nrow(polylinedata()) == 1) {
+            paste("Sorry, insufficient data")
+        } 
+    })
+
+}
+shinyApp(ui, server)
---
+++ us-diplomacy/utils.R
@@ -0,0 +1,100 @@
+setwd("/Users/Sherry/Projects/ImpactHack/")
+library(leaflet)
+library(maps)
+library(rlist)
+
+# leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
+# mapWorld = map("world", fill = TRUE, plot = FALSE)
+# leaflet(data = mapWorld) %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels)
+embassies <- read.csv("full-embassies.csv")
+# leaflet(data = embassies) %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>%
+#   addCircleMarkers(~Longitude, ~Latitude, popup = ~as.character(Mission), 
+#                    label = ~as.character(Name), radius = 5, color = "cyan", 
+#                    stroke = FALSE, fillOpacity = 0.8)
+
+# returns a dataframe of connected countries for addPolylines to use
+connectionsDF <- function(csList, startLat, startLon) {
+  if (typeof(csList) == "character") {
+    countries <- unlist(strsplit(csList, ", "))
+  } else {
+    countries <- csList
+  }
+  linesDF <- data.frame(
+    lat=double(),
+    lon=double()
+  )
+  temp <- data.frame(startLat, startLon)
+  names(temp) <- c("lat", "lon")
+  linesDF <- rbind(linesDF, temp)
+  for (country in countries) {
+    countryInfo <- embassies[embassies$Country==country,3:4]
+    countryLat <- countryInfo[1, 1]
+    countryLon <- countryInfo[1, 2]
+    temp <- data.frame(countryLat, countryLon)
+    names(temp) <- c("lat", "lon")
+    linesDF <- rbind(linesDF, temp)
+  }
+  # linesDF <- linesDF[order(linesDF$lon),]
+  temp <- data.frame(lat=c(38.889248), lon=c(-77.050636))
+  names(temp) <- c("lat", "lon")
+  linesDF <- rbind(linesDF, temp)
+  return(as.data.frame(linesDF[complete.cases(linesDF),]))
+}
+
+# grabs unique values from aggregated column, where each entry is a comma-separated list
+uniqueListCol <- function(colVals) {
+  allValues <- vector("list", 1000)
+  i <- 1
+  for (lst in colVals) {
+    items <- unlist(strsplit(lst, ", "))
+    for (it in items) {
+      allValues[[i]] <- it
+      i <- i + 1
+    }
+  }
+  allValues <- unique(allValues)
+  allValues <- Filter(Negate(is.null), allValues)
+  return(allValues)
+}
+
+allCountries <- uniqueListCol(embassies$Country)
+allTopics <- uniqueListCol(embassies$Topics)
+
+# return a dataframe of countries relating to topic
+topicCountries <- function(ctryTbl, topic) {
+  relevant <- ctryTbl[grepl(topic, embassies$Topics),]
+  return (relevant)
+}
+
+# returns the embassies dataframe filtered by country name and any connections the country has
+connectedCountries <- function(countryName) {
+  correctCountry <- embassies[embassies$Country == countryName,]
+  allConnections <- uniqueListCol(correctCountry[,5])
+  for (c in allConnections) {
+    connCountryData <- embassies[embassies$Country == c,]
+    correctCountry <- rbind(correctCountry, connCountryData)
+  }
+  return(correctCountry)
+}
+
+# csList <- embassies[1, 5]
+# startLat <- embassies[1, 3]
+# startLon <- embassies[1, 4]
+# testAfg <- connectionsDF(csList, startLat, startLon)
+
+# leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>%
+#   addCircleMarkers(data = embassies, ~Longitude, ~Latitude, popup = ~as.character(Mission),
+#                    label = ~as.character(Name), radius = 5, color = "cyan",
+#                    stroke = FALSE, fillOpacity = 0.8) %>%
+#   addPolylines(data = testAfg, lat = ~lat, lng= ~lon, weight = 2, color = "cyan")
+
+# eduTest <- topicCountries("education")
+# eduCountries <- c(eduTest$Country, uniqueListCol(eduTest$Connections))
+# eduConnections <- connectionsDF(eduCountries, eduTest[1, 3], eduTest[1, 4])
+# names(embassies)
+# 
+# leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>%
+#   addCircleMarkers(data = embassies, ~Longitude, ~Latitude, popup = ~as.character(Mission),
+#                    label = ~as.character(Property.Name), radius = 5, color = "cyan",
+#                    stroke = FALSE, fillOpacity = 0.8) %>%
+#   addPolylines(data = eduConnections, lat = ~lat, lng= ~lon, weight = 2, color = "darkblue")
